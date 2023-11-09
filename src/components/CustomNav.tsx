733cdaa77e26120d019c15b7207cefb68e7ad32c
Ygor Garofalo <igorgarofalo@gmail.com>
2023-11-09 16:31:55 +0100

    fix on navbar

--- src/components/CustomNav.tsx
+++ src/components/CustomNav.tsx
@@ -1,14 +1,18 @@
 import { Navbar, Container, Nav } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 
 const CustomNav = () => {
+  const location = useLocation();
+
   return (
     <>
       <Navbar bg="dark" data-bs-theme="dark">
         <Container fluid>
           <Navbar.Brand href="#home">Space News</Navbar.Brand>
           <Nav className="me-auto">
-            <Link to="/">Home</Link>
+            <Link to="/" className={location.pathname === "/" ? "fw-bold" : ""}>
+              Home
+            </Link>
             <Link to="#">Features</Link>
             <Link to="#">About</Link>
           </Nav>
